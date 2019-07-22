34762f8476113d320e6b7bfab6787e6972bfd994
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-22 04:21:59 -0700

    attaching frontend to git repo

---
+++ build/precache-manifest.a7cb7f80208fdee548f147a9e48dc1b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f2c3a297214bf2f8f6e0a9fa93e0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a91ad28bb8163ae7e8",
+    "url": "/static/css/main.d7726732.chunk.css"
+  },
+  {
+    "revision": "bda11d725b3cac28f919",
+    "url": "/static/js/2.c7f2c455.chunk.js"
+  },
+  {
+    "revision": "00a91ad28bb8163ae7e8",
+    "url": "/static/js/main.1079b05d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
